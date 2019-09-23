0f0e02d32411e5ad87c51fe088c57d23311480b6
Ruan Arcega <ruanarcega@gmail.com>
2019-09-23 09:34:25 -0300

    feat: make greetingPrefix function with switch statement

--- src/github.com/rca0/hello/hello.go
+++ src/github.com/rca0/hello/hello.go
@@ -13,17 +13,21 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 
 	case french:
 		prefix = frenchHelloPreffix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return prefix + name
+	return
 }
 
 func main() {
