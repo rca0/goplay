bc088037d81bde4772752f91bbbabfca32a4af71
Ruan Arcega <ruanarcega@gmail.com>
2019-09-25 10:10:13 -0300

    feat: add benchmark example testing

--- src/github.com/rca0/iteration/repeat_test.go
+++ src/github.com/rca0/iteration/repeat_test.go
@@ -10,3 +10,13 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("Expected %q but got %q", expected, repeated)
 	}
 }
+
+// When the benchmark code is executed, it runs "b.N" times
+// and measures how long it takes on nanoseconds.
+// To run the benchmarks: go test -bench=.
+// By default Benchmarks are run sequentially
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
