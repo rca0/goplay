b52e3d7d93e2259ca1cd23281b1fb67aaf85f70c
Ruan Arcega <ruanarcega@gmail.com>
2019-09-25 10:03:49 -0300

    feat: refactor repeat func

--- src/github.com/rca0/iteration/repeat.go
+++ src/github.com/rca0/iteration/repeat.go
@@ -1,10 +1,12 @@
 package iteration
 
+const repeatCount = 5
+
 // Repeat take a string and returns a batch of strings
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
 	}
 	return repeated
 }
